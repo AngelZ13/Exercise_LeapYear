f193015551bf8c486506611f10a31cd4656668ff
AngelZaf8 <73531566+AngelZaf8@users.noreply.github.com>
2021-06-26 21:32:36 +0300

    Add project files.

---
+++ Exercise_LeapYear/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeapYear(1923);
+            LeapYear(2028);
+
+        }
+
+        private static void LeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                Console.WriteLine($"{year} is a leap year.");
+            else
+                Console.WriteLine($"{year} is a normal year.");
+        }
+    }
+}
